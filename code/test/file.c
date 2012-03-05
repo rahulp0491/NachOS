4f9bf315c47b75e1f59a9217ac08e05c0692dcab
Rahul P <rahulp0491@gmail.com>
2012-03-05 20:21:38 +0530

    Added test code with Read returning the actual bytes read

--- code/test/file.c
+++ code/test/file.c
@@ -2,13 +2,17 @@
 
 void main(){
 	char buf[100];
-	OpenFileId fd = Open("testing_file.txt");
-	Write("Hello!", sizeof("Hello!"), fd);
-	Close(fd);
-	fd = Open("testing_file.txt");
-	Read(buf, (sizeof("Hello!")-1), fd);
+	int size;
+	OpenFileId fd = Open("file.txt");
+	OpenFileId fd1 = Open("file1.txt");
+	Write("Hello!", 7, fd);
+	fd = Open("file.txt");
+	size = Read(buf, 100, fd);
+	Write(buf, size, fd1);
 	buf[99] = '\0';
 	Print(buf);
+	Close(fd);
+	Close(fd1);
 	Print("\n");
 	Halt();
 }
